48fc7bfad3034f4ebed9cf6617975b4e59847e78
LilStpd <4646rc.if@gmail.com>
2025-09-16 17:55:39 +0400

    Add fonts

--- app/src/main/java/com/beginina/chatapp/ui/theme/Type.kt
+++ app/src/main/java/com/beginina/chatapp/ui/theme/Type.kt
@@ -2,33 +2,42 @@ package com.beginina.chatapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.beginina.chatapp.R
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily(Font(R.font.montserrat_regular)),
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 24.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val h1 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.montserrat_medium)),
+    fontSize = 24.sp,
+    lineHeight = 24.sp
+)
+val h2 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.montserrat_medium)),
+    fontSize = 18.sp,
+    lineHeight = 24.sp
+)
+val b1 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.montserrat_regular)),
+    fontSize = 16.sp,
+    lineHeight = 24.sp
+)
+val subtitle = TextStyle(
+    fontFamily = FontFamily(Font(R.font.montserrat_medium)),
+    fontSize = 14.sp,
+    lineHeight = 24.sp
+)
+val button = TextStyle(
+    fontFamily = FontFamily(Font(R.font.montserrat_semibold)),
+    fontSize = 16.sp,
+    lineHeight = 24.sp
 )
